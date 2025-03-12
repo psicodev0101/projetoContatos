d874082f7002395d224c4f1a456f956ecfe8b5f8
psicoder0101 <pedrocsouza97@gmail.com>
2025-03-12 11:16:55 -0300

    updated ContatoController class ok

--- contatos-manager/src/main/java/br/com/uniciv/contatos_manager/ContatoController.java
+++ contatos-manager/src/main/java/br/com/uniciv/contatos_manager/ContatoController.java
@@ -1,7 +1,26 @@
 package br.com.uniciv.contatos_manager;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ContatoController {
+    @Autowired
+    private ContatoRepository repository;
+
+    @GetMapping("/contato")
+    public List<Contato> todosContatos(){
+        return repository.findAll();
+    }
+
+    @PostMapping("/contato")
+    public Contato salvar (@RequestBody Contato contato){
+        return repository.save(contato);
+    }
+
 }
